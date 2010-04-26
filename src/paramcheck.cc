a60be952dc68b55e75f924d37cf0849242657584
schnetter <schnetter@53d01c76-9b5b-45fe-803d-9063f4e27562>
2010-04-26 22:11:30 +0000

    Update
    git-svn-id: https://svn.cct.lsu.edu/repos/numrel/LSUThorns/Refluxing/trunk@6 53d01c76-9b5b-45fe-803d-9063f4e27562

---
+++ src/paramcheck.cc
@@ -0,0 +1,21 @@
+#include <cctk.h>
+#include <cctk_Arguments.h>
+#include <cctk_Parameters.h>
+
+extern "C"
+void Refluxing_ParamCheck (CCTK_ARGUMENTS)
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+  
+  if (CCTK_IsThornActive ("CarpetRegrid2")) {
+    CCTK_INT const snap_to_coarse =
+      * static_cast<CCTK_INT const *>
+      (CCTK_ParameterGet ("snap_to_coarse", "CarpetRegrid2", 0));
+    if (not snap_to_coarse) {
+      // Setting this parameter is not really required, but it can't
+      // hurt
+      CCTK_PARAMWARN ("Refluxing requires that the fine grid boundaries are aligned with coarse grid boundaries.  Set CarpetRegrid2::snap_to_coarse to ensure this.");
+    }
+  }
+}
